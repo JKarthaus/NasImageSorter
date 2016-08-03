63fed7d838b009a0a8fa3faf5ee16d892e2dd245
Joern Karthaus <joern.karthaus@gmail.com>
2016-08-03 23:21:50 +0200

    Initial Project Setup

---
+++ de/jkarthaus/NasImageSorter.java
@@ -0,0 +1,12 @@
+package de.jkarthaus;
+
+public class NasImageSorter {
+
+	static Logger log = Logger.getLogger(NasImageSorter.class.getName());
+
+	public static void main(String[] args) {
+		log.debug("Hello this is an debug message");
+		log.info("Hello this is an info message");
+	}
+
+}
---
+++ de/jkarthaus/worker/Worker.java
@@ -0,0 +1,5 @@
+package de.jkarthaus.worker;
+
+public class Worker {
+
+}
